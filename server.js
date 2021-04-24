8e9dff8e5bba181ff8521f6bd73dab856dc04f9f
Robert-Andrei Pandele <r.pandele38@gmail.com>
2021-04-24 22:22:17 +0300

    Added new article route + integration of mongodb with mongoose

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const articleRouter = require('./routes/articles');
-const app = express()
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: false }));
 app.use('/articles', articleRouter);
+
 app.get('/', (req, res) => {
     const articles = [{
         title: "Test title",
