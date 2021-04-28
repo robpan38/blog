ccda4ef22b09537d605236f8aef535efb4622f5e
Robert Pandele <r.pandele38@gmail.com>
2021-04-28 21:56:56 +0300

    added read me feature for each blog post

--- server.js
+++ server.js
@@ -2,25 +2,17 @@ const express = require('express');
 const articleRouter = require('./routes/articles');
 const mongoose = require('mongoose');
 const app = express();
+const Article = require('./models/article');
 
-mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 app.use('/articles', articleRouter);
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: "Test title",
-        createdAt: new Date(),
-        description: "o mare zi pentru omenire"
-    },
-    {
-        title: "Test title2",
-        createdAt: new Date(),
-        description: "o mare zi pentru omenire2"
-    }]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' });
     res.render('articles/index', { articles: articles });
 })
 
