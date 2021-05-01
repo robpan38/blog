8bacdbb59a458dc2d39210f2cf1c0df7a20d227a
Robert-Andrei Pandele <r.pandele38@gmail.com>
2021-05-01 15:57:53 +0300

    added edit functionality

--- routes/articles.js
+++ routes/articles.js
@@ -3,13 +3,33 @@ const Article = require('../models/article');
 const router = express.Router();
 
 router.get('/new', (req, res) => {
-    res.render('articles/new', {article: new Article()});
+    res.render('articles/new', { article: new Article() });
+})
+
+router.get('/edit/:id', async (req, res) => {
+    let article = await Article.findById(req.params.id);
+    res.render('articles/edit', { article: article });
 })
 
 router.get('/:slug', async (req, res) => {
     let article = await Article.findOne({ slug: req.params.slug });
     if (article == null) res.redirect('/');
-    res.render('articles/show', {article: article});
+    res.render('articles/show', { article: article });
+})
+
+router.put('/:id', async (req, res) => {
+    let article = await Article.findById(req.params.id);
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
+
+    try {
+        article = await article.save();
+        res.redirect(`/articles/${ article.slug }`);
+    }
+    catch (e) {
+        res.render('articles/edit', { article: article });
+    }
 })
 
 router.post('/', async (req, res) => {
@@ -21,11 +41,16 @@ router.post('/', async (req, res) => {
 
     try {
         article = await article.save();
-        res.redirect(`/articles/${article.slug}`);
+        res.redirect(`/articles/${ article.slug }`);
     }
     catch (e) {
-        res.render('articles/new', {article: article});
+        res.render('articles/new', { article: article });
     }
+})
+
+router.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id);
+    res.redirect('/');
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const articleRouter = require('./routes/articles');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const app = express();
 const Article = require('./models/article');
 
 mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 app.set('view engine', 'ejs');
-
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
+
 app.use('/articles', articleRouter);
+
 
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({ createdAt: 'desc' });
